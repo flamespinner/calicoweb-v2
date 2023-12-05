9feccf4f598717fdae2c5bf7c675b85ede1f8a27
Michael Wilke <mwilke@enarchtech.com>
2023-12-05 09:35:45 -0600

    Fixed scrollbar on main page

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,8 @@ export default function RootLayout({ children }) { //children prop = whatever is
           autoPlay
           muted
           loop
-          className="absolute z-[-10] w-auto min-w-full min-h-full object-cover"
+          className="absolute z-[-10] w-full h-full object-cover"
+          /*className="absolute z-[-10] w-auto min-w-full min-h-full object-cover"*/
         />
         <Navbar />
         {children}
